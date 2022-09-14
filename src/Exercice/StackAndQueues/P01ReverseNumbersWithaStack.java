82c549a72a23390e7ce3adb3c6f3c6d87267db4f
Boris <b.todorov91@gmail.com>
2022-09-14 11:50:43 +0300

    Create P01ReverseNumbersWithaStack.java

---
+++ src/Exercice/StackAndQueues/P01ReverseNumbersWithaStack.java
@@ -0,0 +1,11 @@
+package Exercice.StackAndQueues;
+
+import java.util.Scanner;
+
+public class P01ReverseNumbersWithaStack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
