3dce9cd16750b1624f46a7923e7250eaa542d98f
Boris <b.todorov91@gmail.com>
2022-09-19 13:29:21 +0300

    new task

---
+++ src/Lab/MultidimensionalArrays/demo.java
@@ -0,0 +1,13 @@
+package Lab.MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
+}
