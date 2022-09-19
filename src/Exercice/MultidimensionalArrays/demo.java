dfa69889b7e69c74259f37530ff6c6f78dd27d25
Boris <b.todorov91@gmail.com>
2022-09-19 22:09:48 +0300

    new

---
+++ src/Exercice/MultidimensionalArrays/demo.java
@@ -0,0 +1,13 @@
+package Exercice.MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
+}
