501dde0fdbe816ae86040a377db06d20bcd3d528
Boris <b.todorov91@gmail.com>
2022-09-25 16:15:11 +0300

    new task

---
+++ src/Exercice/MultidimensionalArrays/P01FillTheMatrix.java
@@ -0,0 +1,68 @@
+package Exercice.MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class P01FillTheMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        /*
+        [0][0] [1][0] [2][0]
+        [0][1] [1][1] [2][1]
+        [0][2] [1][2] [2][2]
+         */
+        /*
+        [0][0] [0][1] [0][2]
+        [1][0] [1][1] [1][2]
+        [2][0] [2][1] [2][2]
+         */
+
+        String[] input = scanner.nextLine().split(", ");
+        int n = Integer.parseInt(input[0]);
+        String pattern = input[1];
+
+        int counter = 1;
+
+        int[][] matrix = new int[n][n];
+
+        if (pattern.equals("A")) {
+            paternA(n, counter, matrix);
+        } else {
+
+            paternB(n, counter, matrix);
+        }
+
+        for (int[] element : matrix) {
+            for (int item : element) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void paternB(int n, int counter, int[][] matrix) {
+        for (int col = 0; col < n; col++) {
+            if (col % 2 == 0) {
+                for (int row = 0; row < n; row++) {
+                    matrix[row][col] = counter;
+                    counter++;
+                }
+            } else {
+                for (int row = n - 1; row >= 0; row--) {
+                    matrix[row][col] = counter;
+                    counter++;
+                }
+            }
+        }
+    }
+
+    private static void paternA(int n, int counter, int[][] matrix) {
+        for (int col = 0; col < n; col++) {
+            for (int row = 0; row < n; row++) {
+                matrix[row][col] = counter;
+                counter++;
+            }
+        }
+    }
+}
--- src/Exercice/MultidimensionalArrays/demo.java
+++ src/Exercice/MultidimensionalArrays/demo.java
@@ -7,6 +7,13 @@ public class demo {
         Scanner scanner = new Scanner(System.in);
 
 
+        int[][] matrix = new int[3][3];
+
+        for (int c = 0; c < matrix.length; c++) {
+            for (int r = 0; r < matrix[c].length; r++) {
+                matrix[c][r] = scanner.nextInt();
+            }
+        }
 
 
     }
