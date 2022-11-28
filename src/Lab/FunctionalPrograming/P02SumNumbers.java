e7f3ba52a30d8b1c64f77896d6345efe21d38bcc
Boris <b.todorov91@gmail.com>
2022-11-28 20:17:41 +0200

    new task

---
+++ src/Lab/FunctionalPrograming/P02SumNumbers.java
@@ -0,0 +1,25 @@
+package Lab.FunctionalPrograming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class P02SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Function<int[], String> count = elements -> "Count = " +
+                Arrays.stream(elements).count();
+
+        Function<int[], String> sum = elements -> "Sum = " +
+                Arrays.stream(elements).sum();
+
+        int[] array = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+
+        System.out.println(count.apply(array));
+        System.out.println(sum.apply(array));
+    }
+}
--- src/Lab/SetsAndMapsAdvanced/demo.java
+++ src/Lab/SetsAndMapsAdvanced/demo.java
@@ -1,20 +1,13 @@
 package Lab.SetsAndMapsAdvanced;
 
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class demo {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Set<Integer> numbers = new HashSet<>();
 
-        numbers.add(13);
-        numbers.add(13);
-
-        System.out.println();
 
 
     }
