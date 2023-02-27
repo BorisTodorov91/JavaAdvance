94906e29db97047665f100acc30b2af05bf95851
Boris <b.todorov91@gmail.com>
2023-02-27 12:38:55 +0200

    new task

--- src/Algorithms/BubbleSort/main.java
+++
@@ -1,4 +0,0 @@
-package Algorithms.BubbleSort;
-
-public class main {
-}
---
+++ src/Algorithms/SortingAlgorithms/BubbleSort.java
@@ -0,0 +1,11 @@
+package Algorithms.SortingAlgorithms;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
