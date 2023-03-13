4ae5b8ffcfe9d1f84a895bc861799f10cca4bb5d
Boris <b.todorov91@gmail.com>
2023-03-13 22:54:43 +0200

    new task

--- src/workshop/CustumStack/CustomStack.java
+++ src/workshop/CustumStack/CustomStack.java
@@ -3,13 +3,63 @@
  * Copyright (c) 2023 Boris T.
  */
 
-package workshop.CustumStack;
 
 /**
  * Custom implementation of LIFO data structure
  * w
  */
+package workshop.CustumStack;
+
+import java.util.function.Consumer;
+
 public class CustomStack {
 
+    private Node top;
+    private int size;
 
+    public void push(int element) {
+        Node newNode = new Node(element, this.top);
+        newNode.prev = top;
+        this.top = newNode;
+        size++;
+    }
+
+    public int pop() {
+        ensureNotEmpty();
+
+        int value = top.element;
+        top = top.prev;
+        size--;
+        return value;
+    }
+
+    public int peek() {
+
+        ensureNotEmpty();
+        return top.element;
+    }
+
+    public void forEach(Consumer<Integer> consumer) {
+        Node current = top;
+        while (current != null) {
+            consumer.accept(current.element);
+            current = current.prev;
+        }
+    }
+
+    private void ensureNotEmpty() {
+        if (top == null) {
+            throw new IllegalArgumentException("Stack is empty!");
+        }
+    }
+
+    private static class Node {
+        private final int element;
+        private Node prev;
+
+        private Node(int element, Node prev) {
+            this.element = element;
+            this.prev = prev;
+        }
+    }
 }
--- src/workshop/CustumStack/main.java
+++ src/workshop/CustumStack/main.java
@@ -10,6 +10,17 @@ public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        CustomStack customStack = new CustomStack();
+        customStack.push(12);
+        customStack.push(13);
+        customStack.push(15);
+        customStack.push(11);
+        customStack.push(10);
+        customStack.push(1);
+        customStack.push(12333);
 
+        customStack.forEach(System.out::println);
+
+        System.out.println();
     }
 }
